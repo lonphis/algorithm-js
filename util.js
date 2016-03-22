e8a3bb6f9b9afbcac4106294bce175d80ea49cff
lonphis <lonphis@gmail.com>
2016-03-22 17:50:06 +0800

    utility function

---
+++ util.js
@@ -0,0 +1,16 @@
+if (typeof Array.prototype.reduce != "function") {
+  Array.prototype.reduce = function (callback, initialValue ) {
+     var previous = initialValue, k = 0, length = this.length;
+     if (typeof initialValue === "undefined") {
+        previous = this[0];
+        k = 1;
+     }
+     
+    if (typeof callback === "function") {
+      for (k; k < length; k++) {
+         this.hasOwnProperty(k) && (previous = callback(previous, this[k], k, this));
+      }
+    }
+    return previous;
+  };
+}
